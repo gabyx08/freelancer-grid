fe0ca896a883ff883576b1cb28ff6666ba88ec6c
fbk-rena <gaby.ranarz@gmail.com>
2017-05-04 18:30:58 -0500

    funcionalidad scroll - nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /*
  * Archivo principal de JS
  */
+
+window.addEventListener("scroll", cambioTamaño);
+
+function cambioTamaño(){
+  console.log(event);
+  var tituloNav = document.getElementsByClassName("tituloHeader");
+  var header = document.getElementsByTagName("header");
+  if (document.body.scrollTop > 100){
+    tituloNav[0].style.fontSize="20px";
+    header[0].style.padding= "8px 15px";
+  }
+  if (document.body.scrollTop < 100){
+    tituloNav[0].style.fontSize="30px";
+    header[0].style.padding= "15px 15px";
+  }
+}
